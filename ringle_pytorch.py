9c56ed19cb7d8efd4b3948194febdc160c510644
jongwooklee <jongwookaist@gmail.com>
2024-02-24 11:18:00 +0900

    ringle_pytorch2

--- ringle_pytorch.py
+++ ringle_pytorch.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 data = [[1, 2],[3, 4]]
@@ -35,10 +36,8 @@ r2 = torch.rand(3, 2)
 
 ###############################################
 #LeNet-5
-import torch                     # PyTorch 모든 모듈 가져오기
 import torch.nn as nn            # torch.nn.Module의 경우 PyTorch model의 부모 객체
 import torch.nn.functional as F  # 활성화 함수 모듈 가져오기
-
 class LeNet(nn.Module):
 
     def __init__(self):
@@ -68,7 +67,7 @@ class LeNet(nn.Module):
         for s in size:
             num_features *= s
         return num_features
-
+    
 net = LeNet()
 print(net)                         # 인스턴스한 객체를 출력하면 어떤 값을 보여줄까요?
 
@@ -81,166 +80,41 @@ print('\n:결과 값')
 print(output)
 print(output.shape)
 
-
-#%matplotlib inline
-
+import torch
 import torchvision
 import torchvision.transforms as transforms
 
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+def main():
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=transform)
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                            download=True, transform=transform)
 
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                          shuffle=True, num_workers=2)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                            shuffle=True, num_workers=2)
+    import matplotlib.pyplot as plt
+    import numpy as np
 
+    classes = ('plane', 'car', 'bird', 'cat',
+            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-
-
-import matplotlib.pyplot as plt
-import numpy as np
-
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-def imshow(img):
-    img = img / 2 + 0.5     # 역정규화
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    def imshow(img):
+        img = img / 2 + 0.5     # 역정규화
+        npimg = img.numpy()
+        plt.imshow(np.transpose(npimg, (1, 2, 0)))
 
 
-# 무작위로 학습 이미지 추출하기
-dataiter = iter(trainloader)
-images, labels = next(dataiter)
+    # 무작위로 학습 이미지 추출하기
+    dataiter = iter(trainloader)
+    images, labels = next(dataiter)
 
-# 이미지 보여주기
-imshow(torchvision.utils.make_grid(images))
-# labels 출력하기
-print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+    # 이미지 보여주기
+    imshow(torchvision.utils.make_grid(images))
+    # labels 출력하기
+    print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
-
-
-
-
-# transform = transforms.Compose(
-#     [transforms.ToTensor(),
-#      transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-# trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-#                                         download=True, transform=transform)
-# trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-#                                           shuffle=True, num_workers=2)
-
-# testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-#                                        download=True, transform=transform)
-# testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-#                                          shuffle=False, num_workers=2)
-
-# classes = ('plane', 'car', 'bird', 'cat',
-#            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-
-
-
-
-# import matplotlib.pyplot as plt
-# import numpy as np
-
-# # 아래 함수는 이미지를 시각화하여 보여주는 함수입니다.
-
-
-# def imshow(img):
-#     img = img / 2 + 0.5     # 역정규화
-#     npimg = img.numpy()
-#     plt.imshow(np.transpose(npimg, (1, 2, 0)))
-
-
-# # 무작위로 학습 이미지를 가져오기
-# dataiter = iter(trainloader)
-# images, labels = next(dataiter)
-
-# # 이미지 시각화하기
-# imshow(torchvision.utils.make_grid(images))
-# # 정답 label 출력하기
-# print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
-
-
-
-
-# class Net(nn.Module):
-#     def __init__(self):
-#         super(Net, self).__init__()
-#         self.conv1 = nn.Conv2d(3, 6, 5)
-#         self.pool = nn.MaxPool2d(2, 2)
-#         self.conv2 = nn.Conv2d(6, 16, 5)
-#         self.fc1 = nn.Linear(16 * 5 * 5, 120)
-#         self.fc2 = nn.Linear(120, 84)
-#         self.fc3 = nn.Linear(84, 10)
-
-#     def forward(self, x):
-#         x = self.pool(F.relu(self.conv1(x)))
-#         x = self.pool(F.relu(self.conv2(x)))
-#         x = x.view(-1, 16 * 5 * 5)
-#         x = F.relu(self.fc1(x))
-#         x = F.relu(self.fc2(x))
-#         x = self.fc3(x)
-#         return x
-
-
-# net = Net()
-
-
-
-
-
-# criterion = nn.CrossEntropyLoss()
-# optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-
-
-
-# for epoch in range(2):  # 데이터셋을 여러 번 반복 학습합니다.
-
-#     running_loss = 0.0
-#     for i, data in enumerate(trainloader, 0):
-#         # 데이터를 통해 이미지와 정답값을 받습니다.
-#         inputs, labels = data
-
-#         # 초기 기울기 파라미터를 0으로 설정합니다
-#         optimizer.zero_grad()
-
-#         # forward + backward + optimize
-#         outputs = net(inputs)
-#         loss = criterion(outputs, labels)
-#         loss.backward()
-#         optimizer.step()
-
-#         # epoch 및 loss 값을 출력합니다
-#         running_loss += loss.item()
-#         if i % 2000 == 1999:    # 2000 mini-batches 마다 값을 출력합니다
-#             print('[%d, %5d] loss: %.3f' %
-#                   (epoch + 1, i + 1, running_loss / 2000))
-#             running_loss = 0.0
-
-# print('Finished Training')
-
-
-
-# correct = 0
-# total = 0
-# with torch.no_grad():
-#     for data in testloader:
-#         images, labels = data
-#         outputs = net(images)
-#         _, predicted = torch.max(outputs.data, 1)
-#         total += labels.size(0)
-#         correct += (predicted == labels).sum().item()
-
-# print('10000개 테스트 이미지에서 모델 정확도: %d %%' % (
-#     100 * correct / total))
-
-
+if __name__ == '__main__':
+    main()
 
